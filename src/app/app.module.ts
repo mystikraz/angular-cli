d2b632c9ec19f88e7b5d5d17d7d117bc72656d05
raj <royal_raj@outlook.com>
2017-10-18 16:49:40 +0545

    1st commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UserDetailsComponent } from './user/user-details/user-details.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserNewComponent } from './user/user-new/user-new.component';
+import { UserService } from './user/user.service';
+import { TestComponent } from './user/test/test.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserDetailsComponent,
+    UserListComponent,
+    UserNewComponent,
+    TestComponent,
+    UserEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
